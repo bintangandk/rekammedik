7cb99f0b21cfefaf6a10e24556864c1ac455c628
Syahroni12 <ronixria@gmail.com>
2024-08-11 21:29:08 +0700

    revisi exposrt excel

--- controller/expor_excel.php
+++ controller/expor_excel.php
@@ -2,17 +2,17 @@
 session_start();
 include '../koneksi.php';
 $conn = new koneksi();
+
 function filterData(&$str)
 {
     $str = preg_replace("/\t/", "\\t", $str);
     $str = preg_replace("/\r?\n/", "\\n", $str);
     if (strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
 }
+
 $tanggal_awal = $_POST['tanggal_awal'];
 $tanggal_akhir = $_POST['tanggal_akhir'];
 $tanggal_awall = date('Y-m-d', strtotime($tanggal_awal));
-
-// Ubah format tanggal akhir
 $tanggal_akhirr = date('Y-m-d', strtotime($tanggal_akhir));
 
 $filename = "Riwayat file dari tanggal" . $tanggal_awall . "sampai" . $tanggal_akhirr . ".xls";
@@ -23,13 +23,10 @@ if ($tanggal_awall == $tanggal_akhirr) {
     $query = "SELECT * FROM riwayat_file WHERE tanggal = '$tanggal_awall'";
 } else {
     $query = "SELECT * FROM riwayat_file WHERE tanggal BETWEEN '$tanggal_awall' AND '$tanggal_akhirr'";
-    # code...
 }
-
 
 $result = $conn->execute($query);
 $data = $result->fetch_all(MYSQLI_ASSOC);
-
 
 $excelData = implode("\t", array_values($field)) . "\n";
 $no = 1;
@@ -38,10 +35,6 @@ if ($data) {
     foreach ($data as $key) {
         $lineData = array($no++, date('d-m-Y', strtotime($key['tanggal'])), $key['waktu'], $key['file'], $key['nama']);
         $excelData .= implode("\t", array_values($lineData)) . "\n";
-        header("Content-Type: application/vnd.ms-excel");
-        header("Content-Disposition: attachment; filename=$filename");
-
-        echo $excelData;
     }
 } else {
     $excelData .= "No Data Found\n";
@@ -50,6 +43,9 @@ if ($data) {
     exit();
 }
 
+// Move headers before any output
+header("Content-Type: application/vnd.ms-excel");
+header("Content-Disposition: attachment; filename=$filename");
 
-
+echo $excelData;
 exit();
